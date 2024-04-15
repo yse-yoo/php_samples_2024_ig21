0bf6e89d10c21778903b4374404520e2fb2a0924
yse-yoo <youhei_yoshikawa@yse-net.com>
2024-04-15 14:24:29 +0900

    update

--- lesson2/connect_test.php
+++ lesson2/connect_test.php
@@ -3,8 +3,32 @@
 require_once 'env.php';
 
 $db_connection = DB_CONNECTION;
-$db_name = DB_USERNAME;
+$db_name = DB_DATABASE;
 $db_host = DB_HOST;
 $db_port = DB_PORT;
 
 $dsn = "{$db_connection}:dbname={$db_name};host={$db_host};port={$db_port};charset=utf8;";
+$db_user = DB_USERNAME;
+$db_password = DB_PASSWORD;
+
+try {
+    $pdo = new PDO($dsn, $db_user, $db_password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+} catch (PDOException $e) {
+    echo "接続失敗: " . $e->getMessage();
+    exit;
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>接続成功</h2>
+</body>
+</html>
