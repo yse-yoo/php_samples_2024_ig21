de87864da8623be8eb7c3faa13171df9fcdbee70
yse-yoo <youhei_yoshikawa@yse-net.com>
2024-04-15 15:03:33 +0900

    update

--- lesson2/connect_test.php
+++ lesson2/connect_test.php
@@ -2,6 +2,7 @@
 // env.php を読み込む
 require_once 'env.php';
 
+// 設定
 $db_connection = DB_CONNECTION;
 $db_name = DB_DATABASE;
 $db_host = DB_HOST;
@@ -12,6 +13,7 @@ $db_user = DB_USERNAME;
 $db_password = DB_PASSWORD;
 
 try {
+    // PDO接続
     $pdo = new PDO($dsn, $db_user, $db_password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
@@ -19,6 +21,10 @@ try {
     echo "接続失敗: " . $e->getMessage();
     exit;
 }
+
+$sql = "SELECT * FROM users;";
+$stmt = $pdo->query($sql);
+// var_dump($stmt);
 ?>
 
 <!DOCTYPE html>
@@ -30,5 +36,7 @@ try {
 </head>
 <body>
     <h2>接続成功</h2>
+    <h3>SQL</h3>
+    <p><?= $stmt->queryString ?></p>
 </body>
 </html>
